38c2da90053c163dceeed564982bdca26e6896f8
root <root@ip-172-31-32-213.ap-northeast-2.compute.internal>
2023-12-21 16:52:14 +0900

    231221 for merge

--- routes/storybook.js
+++ routes/storybook.js
@@ -44,7 +44,7 @@ router.post('/imageUrl', async (req, res) => {
     });
 
     // 파라미터값 누락 확인
-    if (!req.body.book_id || !req.body.img_url|| !req.body.book_page) {
+    if (!req.body.book_id || !req.body.img_url) {
         return resCode.returnResponseCode(res, 1002, apiName, null, null);
     }
 
