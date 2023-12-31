14ba7b06a7c3ee7f800a495cfee739f3e17c56fa
han223 <rlagksdl96@naver.com>
2023-12-31 21:50:02 +0900

    53번 책장조회 API 쿼리수정2 - 중복조회 방지

--- services/storybookMng.js
+++ services/storybookMng.js
@@ -1105,7 +1105,13 @@ function getAllBooks(query, apiName) {
     text: `
           SELECT sb.book_id, sb.book_name, sbi.book_page, sbi.img_url
           FROM STORYBOOK sb
-          LEFT JOIN STORYBOOK_IMAGE sbi ON sb.book_id = sbi.book_id AND sbi.book_page = 0
+          LEFT JOIN (
+              SELECT book_id, book_page, img_url
+              FROM STORYBOOK_IMAGE
+              WHERE book_page = 0
+              ORDER BY create_at DESC -- 또는 사용 가능한 다른 날짜/시간 기준으로 정렬
+              LIMIT 1
+          ) sbi ON sb.book_id = sbi.book_id
           WHERE sb.space_id = ?;
           `,
     params: [query.space_id],
